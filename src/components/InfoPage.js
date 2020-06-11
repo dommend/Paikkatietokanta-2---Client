37d6e477d9662e5f767783cbc38f6250f7a200ec
dommend <jani@penttinen.fi>
2020-06-11 09:13:59 +0300

    Add files via upload

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -4,7 +4,7 @@ import Diver from '../resources/diver.png'
 document.onkeydown = function (evt) {
   evt = evt || window.event;
   if (evt.keyCode === 220) {
-    window.open (process.env.REACT_APP_ADMIN_BASE_URL + '/add', '_self');
+    window.open(process.env.REACT_APP_ADMIN_BASE_URL + '/add', '_self');
   }
 };
 class Info extends React.Component {
@@ -31,70 +31,67 @@ class Info extends React.Component {
                 <br />
                 <strong>Paikkatietokanta 1.4.9 (Client with Admin)</strong> <br />
                 <small>Huom. Versiossa ei olla eroteltu ylläpitoa ja käyttäjärajapintaa.</small>
-                </a>
+              </a>
             </div>
-            
               <div className="col-sm">
                 <div class="text-center">
                   <img src={Diver} style={{ width: '280px' }} alt="Diver" />
                 </div>
               </div>
             </div>
+            <div class="row space">
 
-
-          <div class="row space">
-
-          <div className="col-sm">
-              
-              <a href="https://github.com/dommend/Paikkatietokanta-2---Client" style={{ marginBottom:'20px', padding: '25px' }} className="btn btn-outline-info btn-block">
-  
-  <span style={{ fontSize: '50px' }} className="material-icons">save</span>
-  <br />
-  <strong>Paikkatietokanta 2.0 (Client)</strong> <br />
-  <small>Käyttäjärajapinta</small>
-  </a>
-              </div>
-  
               <div className="col-sm">
-              <a href="https://github.com/dommend/Paikkatietokanta-2----Admin" style={{ marginBottom:'20px', padding: '25px' }} className="btn btn-outline-info btn-block">
-  
-  <span style={{ fontSize: '50px' }} className="material-icons">save</span>
-  <br />
-  <strong>Paikkatietokanta 2.0 (Admin)</strong> <br />
-  <small>Ylläpito</small>
-  </a>
+
+                <a href="https://github.com/dommend/Paikkatietokanta-2---Client" style={{ marginBottom: '20px', padding: '25px' }} className="btn btn-outline-info btn-block">
+
+                  <span style={{ fontSize: '50px' }} className="material-icons">save</span>
+                  <br />
+                  <strong>Paikkatietokanta 2.0 (Client)</strong> <br />
+                  <small>Käyttäjärajapinta</small>
+                </a>
               </div>
-  
-          </div>
+
+              <div className="col-sm">
+                <a href="https://github.com/dommend/Paikkatietokanta-2----Admin" style={{ marginBottom: '20px', padding: '25px' }} className="btn btn-outline-info btn-block">
+
+                  <span style={{ fontSize: '50px' }} className="material-icons">save</span>
+                  <br />
+                  <strong>Paikkatietokanta 2.0 (Admin)</strong> <br />
+                  <small>Ylläpito</small>
+                </a>
+              </div>
+
+            </div>
 
             <div className="row space">
               <div className="col-sm">
 
-                <hr style={{ margin: '10px 0px 50px 0px'}} />
+                <hr style={{ margin: '10px 0px 50px 0px' }} />
 
                 <h4>Ohjeet sivuston ajamiseen (Paikkatietokanta 2.0 - Client)</h4>
 
                 <p>Ajaekseen sivua, sinulla tulee olla <a href="https://nodejs.org/en/">node.js</a> ja mysql-tietokanta.</p>
-                
+
                 <blockquote>
                   <ol>
                     <li>Aja <code>npm install</code></li>
                     <li>Muuta tietokanta-asetukset tiedostosta <code>db.config.</code> Tiedosto löytyy @ <code>app/config/db.config.js</code></li>
                     <li>Luo esimerkin mukainen <code>.env.local</code> -tiedosto kansion juureen:
                     <blockquote>
-                    <code>PORT=8080 <br />
+                        <code>PORT=8080 <br />
                     REACT_APP_BASE_URL=https://paikkatietokanta.net <br />
                     REACT_APP_ADMIN_BASE_URL=https://admin.paikkatietokanta.net <br />
                     REACT_APP_API_URL=https://paikkatietokanta.net/api/ <br />
                     REACT_APP_FLICKR_API="12345" <br />
                     REACT_APP_FLICKR_USERNAME="12345@N00" <br />
                     REACT_APP_OW_API="12345"</code>
-                    </blockquote>
-                    <small>
-                    <code>REACT_APP_FLICKR_API</code> on Flickr:n api (<a href="https://www.flickr.com/services/apps/create/apply">https://www.flickr.com/services/apps/create/apply</a>)<br />
-                    <code>REACT_APP_FLICKR_USERNAME</code> on Flickr:n käyttäjänimi (<a href="https://help.flickr.com/find-your-flickr-login-id-HytypXj1Q">https://help.flickr.com/find-your-flickr-login-id-HytypXj1Q</a>)<br />
-                    <code>REACT_APP_OW_API</code> on OpenWeather api (<a href="https://openweathermap.org/">https://openweathermap.org/</a>)<br />
-                    Huom! Sivusto käyttää karttoina <a href="https://stadiamaps.com/">Stadiamaps.com</a>:n kartta layeriä. Jotta kartat näkyisivät verkossa oikein, tulee sinun tehdä tunnus palveluun.</small> 
+                      </blockquote>
+                      <small>
+                        <code>REACT_APP_FLICKR_API</code> on Flickr:n api (<a href="https://www.flickr.com/services/apps/create/apply">https://www.flickr.com/services/apps/create/apply</a>)<br />
+                        <code>REACT_APP_FLICKR_USERNAME</code> on Flickr:n käyttäjänimi (<a href="https://help.flickr.com/find-your-flickr-login-id-HytypXj1Q">https://help.flickr.com/find-your-flickr-login-id-HytypXj1Q</a>)<br />
+                        <code>REACT_APP_OW_API</code> on OpenWeather api (<a href="https://openweathermap.org/">https://openweathermap.org/</a>)<br />
+                    Huom! Sivusto käyttää karttoina <a href="https://stadiamaps.com/">Stadiamaps.com</a>:n kartta layeriä. Jotta kartat näkyisivät verkossa oikein, tulee sinun tehdä tunnus palveluun.</small>
                     </li>
                     <li>Aja <code>node.server</code> tai <code>npm run-script dev</code> -- Jos tietokantoja ei ole, ne luodaan. <code>Server.js</code> pyörii portissa 8080.</li>
                     <li>Aja <code>npm start</code> -- Käynnistää sivuston portissa 8081.</li>
@@ -118,9 +115,9 @@ class Info extends React.Component {
                     RewriteEngine On<br />
                     RewriteBase /<br />
                     RewriteRule ^index.html$ - [L]<br />
-                    RewriteCond % &lbrace;REQUEST_FILENAME} !-f<br />
-                    RewriteCond % &lbrace;REQUEST_FILENAME} !-d<br />
-                    RewriteCond % &lbrace;REQUEST_FILENAME} !-l<br />
+                    RewriteCond % &#x0007B;REQUEST_FILENAME&#x0007D; !-f<br />
+                    RewriteCond % &#x0007B;REQUEST_FILENAME&#x0007D; !-d<br />
+                    RewriteCond % &#x0007B;REQUEST_FILENAME&#x0007D; !-l<br />
                     RewriteRule . /index.html [L]<br />
                     &lt;/IfModule&gt;
                   </code>
@@ -130,7 +127,7 @@ class Info extends React.Component {
                 <p>Tämä kytkee aikaisemmin määrittämät rewrite-arvot pois api-kansiosta.</p>
                 <code>RewriteEngine Off</code>
 
-                <hr style={{ margin: '50px 0px 50px 0px'}} />
+                <hr style={{ margin: '50px 0px 50px 0px' }} />
 
                 <h4>Ohjeet sivuston ajamiseen (Paikkatietokanta 2.0 - Admin)</h4>
 
@@ -138,20 +135,14 @@ class Info extends React.Component {
 
                 <blockquote>
                   <code>
-                  PORT=8080 <br />
+                    PORT=8080 <br />
                   REACT_APP_BASE_URL=https://paikkatietokanta.net <br />
                   REACT_APP_ADMIN_BASE_URL=https://admin.paikkatietokanta.net
                   </code>
                 </blockquote>
 
                 <p>Huom. Ylläpito ei sisällä kirjautumista tai käyttäjärooleja.</p>
-
-
               </div>
-
-
-
-             
             </div>
           </div>
         </div>
