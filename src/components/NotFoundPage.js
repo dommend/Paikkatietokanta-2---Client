e5e4d13e4c8271ac40a1d508f37edd3bbee216aa
dommend <jani@penttinen.fi>
2020-07-24 01:21:43 +0300

    Add files via upload

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -34,8 +34,11 @@ class NotFoundPage extends React.Component {
           }}
         />
         <div style={{marginTop: '40px', textAlign: 'center'}}>
+          
           <p><img src={Diver} style={{width: '200px'}} alt="Diver" /></p>
+       
           <h2>Sivua ei löydy</h2>
+
           <p><Link to="/" className="btn">Palaa takaisin etusivulle</Link></p>
         </div>
       </div>
